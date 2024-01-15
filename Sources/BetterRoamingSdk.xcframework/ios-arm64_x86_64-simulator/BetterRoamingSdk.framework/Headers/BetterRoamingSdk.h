243cff683ec8fa024fb6ea13275335c96488441f
Ahmed Abumostafa <ahmed.abumostafa@1global.com>
2024-01-15 17:13:26 +0100

    add simulator framework and fix typo

---
+++ Sources/BetterRoamingSdk.xcframework/ios-arm64_x86_64-simulator/BetterRoamingSdk.framework/Headers/BetterRoamingSdk.h
@@ -0,0 +1,18 @@
+//
+//  BetterRoamingSdk.h
+//  BetterRoamingSdk
+//
+//  Created by Ahmed 1Global on 10.01.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BetterRoamingSdk.
+FOUNDATION_EXPORT double BetterRoamingSdkVersionNumber;
+
+//! Project version string for BetterRoamingSdk.
+FOUNDATION_EXPORT const unsigned char BetterRoamingSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterRoamingSdk/PublicHeader.h>
+
+
